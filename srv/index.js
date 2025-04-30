7b594bbc0398576d21f053df8f375344f751aee0
somnath paul <somnath.sap2020@gmail.com>
2025-04-30 23:52:49 +0530

    saas changes

--- srv/index.js
+++ srv/index.js
@@ -10,7 +10,7 @@ passport.use(new JWTStrategy(xsenv.getServices({ uaa: { tag: "xsuaa" } }).uaa));
 
 app.use(passport.initialize());
 app.use(passport.authenticate("JWT", { session: false }));
-
+app.use(express.json())
 app.get("/", (req, res, next) => {
   res.send("Welcome to employee dashboard");
 });
@@ -21,6 +21,18 @@ app.get("/getemplist", checkScope, (req, res, next) => {
     result.push(empl_list[emp]);
   }
   res.send(result);
+});
+
+app.put("/callback/v1.0/tenants/:tenantId", (req, res) => {
+  let consumerSubdomain = req.body.subscribedSubdomain;
+  let tenantUrl = `https://approuter-emp-list${consumerSubdomain}.cfapps.us10-001.hana.ondemand.com`;
+  res.status(200).send(tenantUrl);
+});
+
+app.delete("/callback/v1.0/tenants/:tenantId", (req, res) => {
+  let consumerSubdomain = req.body.subscribedSubdomain;
+  let tenantUrl = `https://approuter-emp-list${consumerSubdomain}.cfapps.us10-001.hana.ondemand.com`;
+  res.status(200).send(tenantUrl);
 });
 
 app.get("/getempbyname", (req, res, next) => {
